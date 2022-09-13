2c73c9d6ac50159ff2af7d95c1d715ad597d4770
Aksa127 <mf_athalla@yahoo.com>
2022-09-13 12:19:57 +0700

    Initial commit

---
+++ TOKI/Expression and Data Types/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int input;
+int arr;
+
+int main()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        scanf("%d", &input);
+        arr[i] = input:
+        printf("%d\n", input);
+        printf("%d\n", arr[i]);
+    }
+
+return 0;
+}
