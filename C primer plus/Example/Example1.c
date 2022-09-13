b4cffa33c94f9bdb89911609e623dcd7f9f07534
Aksa127 <mf_athalla@yahoo.com>
2022-09-13 19:21:22 +0700

    13/09/2022

---
+++ C primer plus/Example/Example1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int cats;
+
+    printf("how many cats do you have?\n");
+    scanf("%d", &cats);
+    printf("You have %d cat(s)!\n", cats);
+
+    return 0;
+}
